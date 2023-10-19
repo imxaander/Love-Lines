f1c83f56510178cfaa65e7469dbd0e4c0cfa3400
imxaander <xander22.ison@gmail.com>
2023-10-19 12:38:39 +0800

    idk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SignUp from './SignUp';
+import SignUp from './components/SignUp';
 
 function App() {
   
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {auth, provider} from "./firebase";
+import {auth, provider} from "../firebase";
 import { signInWithPopup } from "firebase/auth";
 
 function SignUp(){
